802638d0e2efba30a81de4b8a767495a9e66b420
Tony Wang <tonywa@Tonys-MacBook-Pro.local>
2018-02-24 17:51:57 -0500

    add boundary

--- js/app.js
+++ js/app.js
@@ -1,15 +1,49 @@
+// Base class for enemy and player
+var Movable = function(x, y, sprite) {
+    // Location
+    this.x = x;
+    this.y = y;
+    // Image
+    this.sprite = sprite;
+
+    // Constants
+    this.X_MIN = -50;
+    this.X_MAX = 450;
+    this.Y_MIN = -50;
+    this.Y_MAX = 450;
+};
+
+// function that checks whether movable object is allowed
+// to move
+Movable.prototype.canMove = function(xSpeed, ySpeed) {
+    if (xSpeed) {
+        var targetX = this.x + xSpeed;
+        return targetX < this.X_MAX && targetX > this.X_MIN;
+    }
+
+    if (ySpeed) {
+        var targetY = this.y + ySpeed;
+        return targetY < this.Y_MAX && targetY > this.Y_MIN;
+    }
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.speed = 10;
-    this.x = 0;
-    this.y = 0;
+    Movable.call(this, x, y, 'images/enemy-bug.png');
+    this.speed = speed;
+
+    // override base property
+    this.X_MIN = -100;
+    this.X_MAX = 500;
 };
+
+Enemy.prototype = Object.create(Movable.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -17,7 +51,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;
+    if (this.canMove(this.speed * dt, undefined)) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = -100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,18 +69,17 @@ Enemy.prototype.render = function() {
 
 // The player
 var Player = function() {
-    // set image for the player
-    this.sprite = 'images/char-boy.png';
-    this.speed = 10; 
-    this.x = 200;
-    this.y = 420;
+    Movable.call(this, 200, 380, 'images/char-boy.png');
+    this.xSpeed = 100; 
+    this.ySpeed = 80; 
 };
+
+Player.prototype = Object.create(Movable.prototype);
+Player.prototype.constructor = Player;
 
 // Update player's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(x, y) {
-        // this.x += x;
-        // this.y += y;
 };
 
 // Draw the player on the screen, required method for game
@@ -55,34 +92,42 @@ Player.prototype.handleInput = function(key) {
     console.log('key pressed');
     switch(key) {
         case 'left':
-            // this.update(this.speed * -1, 0);
-            this.x -= this.speed;
+            if (this.canMove(this.xSpeed * -1, undefined)) {
+                this.x -= this.xSpeed;
+            }
             break;
         case 'up':
-            // this.update(0, this.speed * -1);
-            this.y -= this.speed;
+            if (this.canMove(undefined, this.ySpeed * -1)) {
+                this.y -= this.ySpeed;
+            }
             break;
         case 'right':
-            // this.update(this.speed, 0);
-            this.x += this.speed;
+            if (this.canMove(this.xSpeed, undefined)) {
+                this.x += this.xSpeed;
+            }
             break;
         case 'down':
-            // this.update(0, this.speed);
-            this.y += this.speed;
+            if (this.canMove(undefined, this.ySpeed)) {
+                this.y += this.ySpeed;
+            }
             break;
         default:
             break;
     }
+    console.log('x = ' + this.x + ' y = ' + this.y);
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
+var enemy1 = new Enemy(-100, 60, 30);
+var enemy2 = new Enemy(-100, 140, 40);
+var enemy3 = new Enemy(-100, 220, 50);
+
 var allEnemies = [
     enemy1,
-    enemy2
+    enemy2,
+    enemy3
 ];
 
 var player = new Player();
